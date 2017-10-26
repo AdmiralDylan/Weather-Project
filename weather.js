ae483af9a088ccdb9270cca449549270dd5fd4c9
AdmiralDylan <dylreitsma@gmail.com>
2017-10-26 14:30:34 -0400

    Update weather.js
    new js

--- weather.js
+++ weather.js
@@ -1,6 +1,4 @@
-var x = document.getElementById("header");
-
-
+var x = document.getElementById("location");
 
 function getLocation() {
     if (navigator.geolocation) {
@@ -9,12 +7,22 @@ function getLocation() {
         x.innerHTML = "Geolocation is not supported by this browser.";
     }
 }
-
 function showPosition(position) {
-    x.innerHTML = "Your position is" + "<br>Latitude: " + position.coords.latitude + 
+	
+x.innerHTML = "Your position is" + "<br>Latitude: " + position.coords.latitude + 
     "<br>Longitude: " + position.coords.longitude;
 }
-
-
-
 getLocation();
+
+var api = 'api.openweathermap.org/data/2.5/weather?';
+var key = '&appid=1368f388c690376bc42b3ca71fd14d90';
+var pos = "lat =" + position.coords.latitude + "&" + "long =" + position.coords.latitude;
+
+
+
+function loadAPI(){
+	var url = api + key + pos;
+	loadJSON(url);
+}
+
+loadAPI();
